a9a0e7b831f0bbc3416ae498decf5fc5cc81bb19
Kaeserkiev <kaeserkiev@gmail.com>
2016-01-07 11:41:41 +0200

    template

---
+++ demo/js/common.js
@@ -0,0 +1,14 @@
+//ie fix
+if(!(window.console && console.log)) {
+	console = {
+		log: function(){},
+		debug: function(){},
+		info: function(){},
+		warn: function(){},
+		error: function(){}
+	};
+}
+
+(function($){
+
+})(jQuery);
